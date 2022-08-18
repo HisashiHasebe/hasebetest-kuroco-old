9ae65611a0d90d1c3234b0731bf17e517491bd67
HisashiHasebe <hhasebe@diverta.co.jp>
2022-08-18 11:45:39 +0800

    Revert "セキュリティ確認初期状態"
    This reverts commit 92ad2f1cee3b58055d3828513226fbd9dcbccf85.

--- pages/test/news/index.vue
+++ pages/test/news/index.vue
@@ -10,11 +10,17 @@
 </template>
 
 <script>
-export default {
-  async asyncData({ $axios }) {
-    return {
-      response: await $axios.$get('/rcms-api/13/news'),
-    };
+ export default {
+     async asyncData ({ $axios }) {
+         try {
+           const response = await $axios.$get('/rcms-api/13/news', {
+               headers: { 'x-rcms-api-access-token': 'b33f52bcdbc6d0b60ccbe43a9c42ffe7e847c00c8aa2e12a80c4c848127e9547' }
+             }
+           )
+             return { response }
+         }catch (e) {
+             console.log(e.message)
+         }
   },
 };
 </script>
